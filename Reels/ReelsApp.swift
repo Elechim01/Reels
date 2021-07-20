42ce75bcb61d7a2318a162b20ba4beac53347b4f
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-20 22:13:01 +0200

    Initial Commit

---
+++ Reels/ReelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelsApp.swift
+//  Reels
+//
+//  Created by Michele Manniello on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
